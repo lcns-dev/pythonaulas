17d3633bc6bc6a29f99304fb2e4a1504cc35652c
Leandro <lcns320.lc@gmail.com>
2022-06-22 20:49:06 -0300

    Att 2.4 Aprender!

--- desafio058.py
+++ desafio058.py
@@ -1,13 +1,17 @@
 #Melhore o jogo do Desafio 028 onde o computador vai pensar em um número de 0 até 10.
 # Só que agora o jogador vai tentar advinhar até acertar, mostrando no final quantos palpites foram foram necessários para vencer.
 
-import random
+from random import randint
 print("-=" * 30)
 print("Vou pensar em um número de 0 até 10, tente adivinhar.")
 print("-=" * 30)
-escolha = int(input("Em que número eu pensei?"))
-pc = random.randint(0, 10)
+pc = randint(0, 10)
 cont = 0
-while escolha != pc:
-    print("Opa")
-print("Tenta novamente")
+acertou = False
+while not acertou:
+    escolha = int(input("Em que número eu pensei?"))
+    if pc == escolha:
+        acertou = True
+    else:
+        print("ERROU!!! Tente outra vez!")
+print("ACERTOU!!!")
